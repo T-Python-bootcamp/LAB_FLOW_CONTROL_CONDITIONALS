546492ed9c10e7cfbdb4658625f7a2936013e38d
Basel Alanazi <alanaziibasel@gmail.com>
2022-02-15 11:09:13 +0300

    Create python file

---
+++ file.py
@@ -0,0 +1,11 @@
+movie = ""
+rating = 3
+popularity = 72.65
+if rating >= 4 and popularity > 80:
+    print ("Highly recommended")
+elif rating >= 3 and  popularity > 70:
+        print ("I recommended it. It is good")
+elif rating <= 2 and  popularity > 60:
+    print ("You should check it out!")
+else:
+    print ("Don't watch it. It is a waste of time")
