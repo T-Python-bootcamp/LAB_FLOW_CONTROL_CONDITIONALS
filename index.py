b7ff068315697886fb3376a2ee5cb871bfd360b7
Mohammmed1101 <92180726+Mohammmed1101@users.noreply.github.com>
2022-02-20 16:23:40 +0300

    Update index.py

--- index.py
+++ index.py
@@ -2,11 +2,11 @@ movie , ratingMovie  = "The fast & Farious" , 3
 rating = 5
 popularityScore = 72.65
 
-if ratingMovie > 4 or popularityScore > 80 :
+if ratingMovie >= 4 and popularityScore > 80 :
     print("Highly recommended")
-elif ratingMovie > 3 or popularityScore > 70 :
+elif ratingMovie >= 3 and popularityScore > 70 :
     print("I recommended it. It is good")
-elif ratingMovie < 2 or popularityScore < 60 :
+elif ratingMovie <= 2 or popularityScore < 60 :
     print("You should check it out!")
 else : 
     print("Don't watch it. It is a waste of time")
