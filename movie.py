840cb2ba8dfa4291eff54ae8a184768fe6e2aa52
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-02-20 16:18:26 +0300

    lab /

---
+++ movie.py
@@ -0,0 +1,15 @@
+movie =  "The Notebook"
+rating = 3
+popularity = 72.65
+
+if rating >= 4 and popularity > 80:
+    print("Highly recommended")
+
+elif rating >= 3 and popularity > 70:
+    print("I recommended it . It is good")
+
+elif rating <= 2 and popularity > 70:
+    print("You should check it out!")
+
+else:
+    print("Don't watch it. It is a waste of time")
