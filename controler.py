2d2095e4542d0abae17df8e19158f36df51ed136
jamelhfarhan <jamelh.farhan.rae.js@tuwaiq.edu.sa>
2022-02-20 16:24:21 +0300

    lab

---
+++ controler.py
@@ -0,0 +1,14 @@
+movie ="me befor you"
+
+reting= 3
+popularity= 72.65
+
+
+if reting >= 4 and popularity >80 :
+    print("  recommended recommended")
+elif reting >= 3 and popularity > 70:
+    print(" recommend it. It's good")
+elif  reting >=2 and  popularity < 60:
+    print("You should check it out") 
+elif reting <=2  and popularity <50:
+ print ("Don't watch it. It is a waste of time")     
