7f2a104e69e594e69db1540825d80151871c6185
OthmanAlrasheedi <othman.mousa.rae.js@tuwaiq.edu.sa>
2022-02-20 16:19:56 +0300

    aftet update

--- labif.py
+++ labif.py
@@ -9,5 +9,5 @@ elif classification <= 3 and score >70 :
     print("I recommended it . It is good")
 elif classification <= 2 and score >60 :
     print("You should check it out!")
-else : classification =2 and score<50
-print ("Don't watch it. It is a waste of time")
+else :
+    print ("Don't watch it. It is a waste of time")
