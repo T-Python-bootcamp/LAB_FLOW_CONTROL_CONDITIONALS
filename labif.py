431531e324a48614b67a8dbb2cc0b486615ff7a7
OthmanAlrasheedi <othman.mousa.rae.js@tuwaiq.edu.sa>
2022-02-20 16:13:50 +0300

    lab if statment

---
+++ labif.py
@@ -0,0 +1,13 @@
+movie="jhon  wick"
+classification=3 
+score=72.65
+
+
+if classification >4 and score > 80 :
+    print("Highly recommended")
+elif classification <= 3 and score >70 :
+    print("I recommended it . It is good")
+elif classification <= 2 and score >60 :
+    print("You should check it out!")
+else : classification =2 and score<50
+print ("Don't watch it. It is a waste of time")
