b9bf9ef8fa4147c2734b4fae2d5fd02da768e61e
Mohammmed1101 <92180726+Mohammmed1101@users.noreply.github.com>
2022-02-20 16:27:52 +0300

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ if ratingMovie >= 4 and popularityScore > 80 :
     print("Highly recommended")
 elif ratingMovie >= 3 and popularityScore > 70 :
     print("I recommended it. It is good")
-elif ratingMovie <= 2 or popularityScore < 60 :
+elif ratingMovie <= 2 and popularityScore < 60 :
     print("You should check it out!")
 else : 
     print("Don't watch it. It is a waste of time")
